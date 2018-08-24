2dc57719985a881f7660eaa0e4ce17cbb0aa91d7
Silas Burger <silas.a.burger@gmail.com>
2018-08-24 15:23:55 -0400

    ready to turn in

--- public/portfolio.js
+++ public/portfolio.js
@@ -1,1 +1,11 @@
-$('.collapse').collapse();
+function copyText() {
+  var copyText = document.getElementById('email');
+  var currentFocus = document.activeElement;
+  var icon = document.getElementById('copy');
+  copyText.value = 'silasburger.co@gmail.com';
+  copyText.select();
+  document.execCommand('copy');
+  currentFocus.focus();
+  copyText.value = '';
+  icon.className = 'fas fa-check';
+}
